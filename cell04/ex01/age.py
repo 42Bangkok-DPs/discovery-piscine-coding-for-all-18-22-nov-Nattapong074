b77857aabc60347e193e4c35c375f7f7d3ce076f
Nattapong074 <67030074@kmitl.ac.th>
2024-11-21 08:53:34 +0000

    upcase_it.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+age = int(input("please tell me your age:"))
+print("You are currently",age,"years old.")
+ageone = age + 10
+agetwo = agr + 20
+agethree = age + 30
+print("In 10 years, you 'll be {ageone}years old.")
+print("In 20 years, you 'll be {agetwo}years old.")
+print("In 30 years, you 'll be {agethree}years old.")
